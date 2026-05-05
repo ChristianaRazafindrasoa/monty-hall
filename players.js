d1699d21c7026bea027532c9a5a2937b6c2003f1
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2026-05-05 16:55:25 -0700

    added two players

---
+++ players.js
@@ -0,0 +1,27 @@
+import { readString, readInt } from "./standard-in.js";
+
+export class HumanPlayer {
+    pickDoor() {
+        const door = readInt("Choose a door [1-3]: ", 1, 3);
+        return door - 1;
+    }
+
+    shouldSwitch() {
+        return readString("Switch doors [y/n]?: ")
+            .toLowerCase() === "y";
+    }
+}
+
+export class ComputerPlayer {
+    constructor(switchDoor) {
+        this.switchDoor = switchDoor;
+    }
+
+    pickDoor() {
+        return Math.floor(Math.random() * 3);
+    }
+
+    shouldSwitch() {
+        return this.switchDoor;
+    }
+}
